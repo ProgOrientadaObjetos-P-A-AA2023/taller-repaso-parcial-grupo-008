8d3716957861633d2609a718e25571b211acd8ed
sebastianmend <sebastianfelipemendietalima@gmail.com>
2023-07-05 12:59:47 -0500

    trabajo enviado

--- EjercicioRepaso/src/paquete005/Principal.java
+++ EjercicioRepaso/src/paquete005/Principal.java
@@ -3,31 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete005;
-import java.util.ArrayList;
 
+
+
+import java.util.ArrayList;
+import paquete001.Persona;
+import paquete002.Ciudad;
 import paquete003.BilleteraPagos;
 import paquete004.Pago;
-
 /**
  *
  * @author reroes
  */
 public class Principal {
+
     public static void main(String[] args) {
-        
-        
-        BilleteraPagos pago = new BilleteraPagos();
-            
-        
-        
-        
-        
-        ArrayList<Pago> lista = new ArrayList<>();
-        lista.add(bus);
-        lista.add(taxi);
-        lista.add(t1);
-        
-        
+
+        ArrayList<Pago> listaPagos = new ArrayList<> ;
+
+        /*private String tipoPagoAguaPotable;
+    private double tarifaFija = 2.20;
+    private double metrosCubicosConsumo = 100.2;
+    private double costoConsumoCubicos = 0.2;
+
+    private double tarifaFija = 2.20;
+    private double metrosCubicosConsumo = 100.2;
+    private double costoConsumoCubicos = 0.2;*/
+        Ciudad ciudad = new Ciudad("Loja");
+        Persona p = new Persona("Luis", "Armijos", 22, "99990", ciudad);
+        BilleteraPagos pago = new BilleteraPagos(p, ciudad, listaPagos);
+
         System.out.println(pago);
     }
 }
